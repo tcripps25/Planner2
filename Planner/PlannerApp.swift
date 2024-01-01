0247be6cf12d359ee8b04216ceaf2531137f3179
Tom <36952502+tcripps25@users.noreply.github.com>
2024-01-01 10:16:26 +0000

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Tom Work on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
