ff08957bb9ce7186baad54f94f179f4284212ef5
Tom <36952502+tcripps25@users.noreply.github.com>
2024-03-01 12:01:49 +0000

    updates

---
+++ Planner/Views/DayScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  DayScrollView.swift
+//  Planner
+//
+//  Created by Tom Work on 29/02/2024.
+//
+
+import SwiftUI
+
+struct DayScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DayScrollView()
+}
